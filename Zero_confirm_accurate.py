ab9056dbf21ede7728ab3bf5fc903e72dd75d97a
Chad Thackray <chadthackray@gmail.com>
2020-03-29 18:35:01 +0100

    Included fast and accurate versions

--- Zero_confirm_accurate.py
+++ Zero_confirm_accurate.py
@@ -26,16 +26,14 @@ psi0 = sym.series(psi0, p, x0 = 0.5, n = 20).removeO()
 c1 = -1/(2**5 * 3 * sym.pi**2)* sym.diff( psi0, p, 3)
 c2 = 1/(2**11 * 3**2 * sym.pi**4) * sym.diff(psi0,p,6) + 1/(2**6 * sym.pi**2 )* sym.diff(psi0, p, 2)
 
+cut_off = 12
 
-
-psi0 = sym.series(psi0, p, x0 = 0.5, n = 12).removeO()
-c1 = sym.series(c1, p, x0 = 0.5, n = 12).removeO()
-c2 = sym.series(c2, p, x0 = 0.5, n = 12).removeO()
+psi0 = sym.series(psi0, p, x0 = 0.5, n = cut_off).removeO()
+c1 = sym.series(c1, p, x0 = 0.5, n = cut_off).removeO()
+c2 = sym.series(c2, p, x0 = 0.5, n = cut_off).removeO()
     
-##### Estimation of remainder term
-remfunc = ( (-1)**(N-1)*(t/(2*sym.pi))**(-1/4) ) * (psi0)  ## only includes C0
-    
-#remfunc = ( (-1)**(N-1)*(t/(2*sym.pi))**(-1/4) ) * (psi0+ c1*(t/(2*sym.pi))**(-1/2) + c2*(t/(2*sym.pi))**(-2/2) ) 
+##### Estimation of remainder term   
+remfunc = ( (-1)**(N-1)*(t/(2*sym.pi))**(-1/4) ) * (psi0+ c1*(t/(2*sym.pi))**(-1/2) + c2*(t/(2*sym.pi))**(-2/2) ) 
 
 
 ###############################################################
@@ -66,7 +64,7 @@ def theta(t):
     return 0.5*t* np.log(t/(2*np.pi)) - (0.5*t) - (np.pi*0.125) + 1/(48*t) + 7/(5760*(t**3))
 
 
-def gram(n): ## make this more accurate at some point
+def gram(n): 
     """
     returns g_n, then nth gram point
     """
@@ -129,7 +127,7 @@ def law(x, extra = 0):
     zlist = Zlist(gramlist + extra)
 
     signs = [ (-1)**n for n in x]
-    errors = 0.127*(gramlist+ extra)**(-3/4)
+    errors = 0.77*(gramlist+ extra)**(-7/4)
 
     toSort = np.multiply(zlist, signs)
     toSort2 = toSort[ toSort < errors]
@@ -245,7 +243,7 @@ def turing_bound(n, step = 0.1):
 ##############                    #############################
 ###############################################################
 
-def Bulklehmans(z, step = 0.1): ## optimal step value important
+def Bulklehmans(z, step = 0.1):
     """
     given a list of gram points that do no follow grams law, determine if there can be found suitable h_n's such that
     (-1)^n Z(g_n + h_n) > err and g_n-1 + h_n-1 < g_n + h_n < g_n+1 + h_n+1
@@ -282,7 +280,7 @@ def verifyBulk(x,y):
             print("Turing bound failed, try a smaller step")
             return 
 
-    difficulties = law([z for z in range(x,y+1)]) ## list of gram points which fail grams law
+    difficulties = law([z for z in range(x,y+1)])
 
     groupsize = 200
     groups = np.array_split(difficulties, np.rint(difficulties.size / groupsize))
